12ed99f05101503fa18bc58d8bbfa07a3d134db9
Kevin Peng <kpeng97@gmail.com>
2015-09-18 19:42:57 -0400

    initialzed window

--- src/groupproj/GroupProj.java
+++ src/groupproj/GroupProj.java
@@ -1,3 +1,4 @@
+package groupproj;
 import java.io.*;
 import java.util.*;
 import java.time.*;
@@ -6,12 +7,16 @@ import java.time.*;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package groupproj;
 
-public class GroupProj {
+public class GroupProj extends JFrame {
 
     public static void main(String[] args) {
-	
+	GroupProj g = new GroupProj();
+	g.setTitle("ALARM");
+	g.setSize(800,600);
+	g.setDefaultCloseOperation(WindowsConstants.EXIT_ON_CLOSE);
+	//put logic here
+	g.setVisible(true);
     }
     
 }
