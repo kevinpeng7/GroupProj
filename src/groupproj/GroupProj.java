8a39317bc3b8922fabeb6c4d8fc918b46ea27ada
Harry-L <harryliu0@hotmail.com>
2015-09-19 00:27:55 -0400

    Added the methods setStartTime(), setEndTime(), and getCurrentCounter()

--- src/groupproj/GroupProj.java
+++ src/groupproj/GroupProj.java
@@ -1,8 +1,9 @@
 package groupproj;
 import java.io.*;
+import java.text.*;
 import java.util.*;
-import java.time;
-import java.javax.*;
+import java.time.*;
+import javax.swing.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,13 +13,42 @@ import java.javax.*;
 
 public class GroupProj extends JFrame {
 
-    public static void main(String[] args) {
+    public static Date startTime; 
+    public static Date endTime; 
+    
+    public static void main(String[] args) throws ParseException {
 	GroupProj g = new GroupProj();
 	g.setTitle("ALARM");
 	g.setSize(800,600);
-	g.setDefaultCloseOperation(WindowsConstants.EXIT_ON_CLOSE);
+	g.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	//put logic here
+        
 	g.setVisible(true);
+        
+        startTime = new Date();
+        endTime = new Date();
+        setStartTime();
+        setEndTime(1500000);
+        Date d = new Date();
+        for (int k = 0; true; k++){
+            d.setTime(getCurrentCounter());
+            System.out.println(d.toString());
+        }
     }
     
+    public static void setStartTime() throws ParseException{
+        Calendar c = Calendar.getInstance();
+        startTime.setTime(c.getTimeInMillis());
+    }
+    
+    public static void setEndTime(long milliseconds) throws ParseException{
+        endTime.setTime(milliseconds + startTime.getTime());
+    }
+    
+    public static long getCurrentCounter(){
+        Calendar C = Calendar.getInstance();
+        long counter = C.getTimeInMillis() - startTime.getTime();
+        
+        return counter; 
+    }
 }
